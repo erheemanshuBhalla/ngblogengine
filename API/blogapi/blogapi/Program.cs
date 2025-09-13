0603d6751f15321b2857772120d05458510f5138
Himanshu Bhalla <erheemanshubhalla@gmail.com>
2025-09-13 17:30:25 +0530

    misc changes

--- API/blogapi/blogapi/Program.cs
+++ API/blogapi/blogapi/Program.cs
@@ -15,6 +15,20 @@ builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange
 // Add Ocelot
 builder.Services.AddOcelot(builder.Configuration);
 
+//JWT Authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => {
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
