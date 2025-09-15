f8a67d8e1aca59eba20ffb76ceb5bfb5b005bf56
Himanshu Bhalla <erheemanshubhalla@gmail.com>
2025-09-15 19:48:58 +0530

    misc

--- Code/Code.Domain/Interfaces/IUserInterface.cs
+++ Code/Code.Domain/Interfaces/IUserInterface.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Code.Domain.Entities
+namespace Code.Domain.Interfaces
 {
-    public class Blogmodel
+    internal interface IUserInterface
     {
-
     }
 }
